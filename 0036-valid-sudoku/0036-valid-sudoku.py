795201bae780b4563a77a1f41b844eabf1764ae9
Mihir Burade <154793958+mihirburade@users.noreply.github.com>
2026-07-26 18:42:27 +0530

    Time: 11 ms (25.54%), Space: 12.3 MB (59.27%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -0,0 +1,61 @@
+class Solution(object):
+    def isValidSudoku(self, board):
+        """
+        :type board: List[List[str]]
+        :rtype: bool
+        """
+        def valid(board):
+        
+        
+            #for rows
+            for rows in range(0,9):
+                seen=set()
+                for col in range(0,9):
+                    value=board[rows][col]
+
+                    if value in seen:
+                        # print("not valid -rows")
+                        return False
+
+                    if value==".":
+                        continue
+
+                    # if value not in seen:
+                    seen.add(value)
+
+
+            #for col
+            for col in range(0,9):
+                seen=set()
+                for row in range(0,9):
+                    value=board[row][col]
+                    if value in seen:
+                        # print("not valid -colums")
+                        return False
+
+                    if value==".":
+                        continue
+
+                    # if value not in seen:
+                    seen.add(value)
+
+
+            #for box
+            for row_box in range(0,9,3):
+                    for col_box in range(0,9,3):
+                        seen=set()    
+                        for i in range(3):
+                            for j in range(3):
+                            
+                                value=board[row_box+i][col_box+j]
+                                if value in seen:
+                                    # print("not valid-box")
+                                    return False
+
+                                if value==".":
+                                    continue
+
+                                # if value not in seen:
+                                seen.add(value)
+            return True
+        return(valid(board))
