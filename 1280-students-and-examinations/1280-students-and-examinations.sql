a8683b693259480fa76629163763577898a8961a
Mihir Burade <154793958+mihirburade@users.noreply.github.com>
2026-08-09 02:20:24 +0530

    Time: 1194 ms (29.18%), Space: 0B (100%) - LeetHub

--- 1280-students-and-examinations/1280-students-and-examinations.sql
+++ 1280-students-and-examinations/1280-students-and-examinations.sql
@@ -8,7 +8,7 @@ cross join Subjects as sub
 left join Examinations as ex
 on stu.student_id  = ex.student_id and sub.subject_name = ex.subject_name
 group by stu.student_id,stu.student_name,sub.subject_name
-order by stu.student_id;
+order by stu.student_id,stu.student_name;
 
 
 
